03a9bda500a5f6f1c769e08e1517fd2af2ef3147
unknown <wlhmyit@126.com>
2014-06-22 01:04:36 +0800

    add blogs & tpl

---
+++ iblog/blog/blogs.js
@@ -0,0 +1,2 @@
+var blogs = [];
+blogs.push(blog_2014_06_21_凌晨1点);
---
+++ iblog/js/main.js
@@ -0,0 +1,25 @@
+
+
+$(function(){
+
+	createBlogs(blogs, 100);
+
+	function createBlogs(blogs, n){
+		var blogs = blogs;
+		var num = n > blogs.length ? blogs.length : n;
+		var element = $('#showblog'); 
+	    var tpl = $('#tpl_blog').html();
+	    var myBlogs = [];
+	    for(var i = 0; i < num; i++){
+	    	myBlogs.push(blogs[i]);
+	    }
+		var data = {  
+		    list: myBlogs
+		}   
+		var html = _.template(tpl, data);
+		element.html(html); 
+	}	
+});
+
+
+ 
