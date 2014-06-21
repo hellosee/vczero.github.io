dcf5e63c729b3d0857c2d7d877c9c9086253c1af
unknown <wlhmyit@126.com>
2014-06-22 01:56:38 +0800

    modify the p's bug

--- iblog/blog/blogs.js
+++ iblog/blog/blogs.js
@@ -4,9 +4,10 @@
 define(function(require, exports, module){
 	var blogs = [];
 
-	var blog_1 =  require('./2014-06-21.js'); //取得外界exs
-	blogs.push(blog_1.blog_2014_06_21_am_1); //调用exs的blog
+	var blog_1 =  require('./2014-06-21.js'); //取得外界exs  -> 调用exs的blog
 
+	blogs.push(blog_1.blog_2014_06_21_am_1);
+	blogs.push(blog_1.blog_2014_06_21_am_2); 
 	
 	exports.blogs = blogs;
 });
