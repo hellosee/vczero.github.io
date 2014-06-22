d21839c9528a3d471ecaee848656ee5bdabc7dda
unknown <wlhmyit@126.com>
2014-06-22 09:18:05 +0800

    add first blog

--- iblog/blog/blogs.js
+++ iblog/blog/blogs.js
@@ -7,7 +7,7 @@ define(function(require, exports, module){
 	var blog_1 =  require('./2014-06-21.js'); //取得外界exs  -> 调用exs的blog
 
 	blogs.push(blog_1.blog_2014_06_21_am_1);
-	blogs.push(blog_1.blog_2014_06_21_am_2); 
+
 	
 	exports.blogs = blogs;
 });
