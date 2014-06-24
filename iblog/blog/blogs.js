ad5a2cfc4c20990feede0b372fdd803619732b7c
unknown <wlhmyit@126.com>
2014-06-24 09:43:37 +0800

    add note

--- iblog/blog/blogs.js
+++ iblog/blog/blogs.js
@@ -4,11 +4,14 @@
 define(function(require, exports, module){
 	var blogs = [];
 
-	var blog_1 =  require('./2014-06-21.js'); //取得外界exs  -> 调用exs的blog
-	var blog_2 = require('./2014-06-24.js');
 
-	blogs.push(blog_1.blog_2014_06_21_am_1);
+	var blog_2 = require('./2014-06-24.js');
+	var blog_1 =  require('./2014-06-21.js'); //取得外界exs  -> 调用exs的blog
+	
+
 	blogs.push(blog_2.blog_2014_06_24);
+	blogs.push(blog_1.blog_2014_06_21_am_1);
+
 
 	
 	exports.blogs = blogs;
