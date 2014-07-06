69cdb506de8492c8a631ce90fa62e3087c88f49e
unknown <wlhmyit@126.com>
2014-07-06 21:56:58 +0800

    add dreams

---
+++ iblog/blog/2014-07-06.js
@@ -0,0 +1,24 @@
+define(function(require, exports, module){
+	var types = require('./type.js');
+
+	var blog_2014_07_06 = {
+		id:'blog_2014_07_06',
+		title: '<a class="title_a" href="article.html?id=blog_2014_07_03">梦想支撑着现实</a>',
+		blog_tags: ['北大宣传片'],
+		blog_time: '2014-07-06 21:52',
+		type: types.type.story,
+		blog_p:[
+			{
+				title: '',
+				content:['微信朋友圈里看到一个北大的宣传片，挺触动的。北大是唯一一个可以做梦的地方，没有人笑话你的梦想。不是现实支撑着梦想，是梦想支撑着现实。']
+			}
+		]
+	};
+
+
+
+	
+	exports.blog_2014_07_06 = blog_2014_07_06;
+
+
+});
