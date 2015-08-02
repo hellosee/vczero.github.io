f85afdce4ee175bf667dfae0017ef39dc9c85540
lh_wang <lh_wang@Ctrip.com>
2015-08-02 09:14:56 +0800

    add lvtu

---
+++ lvtu/js/data.js
@@ -0,0 +1,111 @@
+;(function(window, $){
+	
+	window.routes1 = [
+		{
+			x: 121.456604,
+			y: 31.23394,
+			content: '上海',
+			startTime: '8:00'
+		},
+		{
+			x: 121.14212,
+			y: 31.264466,
+			content: '苏州'
+		},
+		{
+			x: 120.632629,
+			y: 31.28794,
+			content: '无锡'
+		},
+		{
+			x: 120.2742,
+			y: 31.582045,
+			content: '常州'
+		},
+		{
+			x: 119.943237,
+			y: 31.771375,
+			content: '丹阳'
+		},
+		{
+			x: 119.409027,
+			y: 32.196533,
+			content: '镇江'
+		},
+		{
+			x: 119.393921,
+			y: 32.41011,
+			content: '扬州'
+		}
+	];
+	
+	window.routes2 = [
+		{
+			x: 121.406822,
+			y: 31.28794,
+			content: '上海',
+			startTime: '9:00'
+		},
+		{
+			x: 120.651855,
+			y: 31.333698,
+			content: '苏州'
+		},
+		{
+			x: 120.344238,
+			y: 31.619474,
+			content: '无锡'
+		},
+		{
+			x: 119.932251,
+			y: 31.858897,
+			content: '常州'
+		}
+	];
+	
+	window.routes3 = [
+		{
+			x: 121.433945,
+			y: 31.16816,
+			content: '上海',
+			startTime: '6:00'
+		},
+		{
+			x: 120.772705,
+			y: 30.674534,
+			content: '嘉兴'
+		},
+		{
+			x: 120.260468,
+			y: 30.353916,
+			content: '杭州'
+		},
+		{
+			x: 119.687805,
+			y: 29.150962,
+			content: '金华'
+		},
+		{
+			x: 118.834991,
+			y: 28.984718,
+			content: '衢州'
+		}
+	];
+	
+	window.urls = [
+		'http://m.ctrip.com/webapp/you/appshare/bargain/18484.html?from=http%3A%2F%2Fm.ctrip.com%2Fwebapp%2Fyou%2Fplace%2F12.html',
+		'http://m.ctrip.com/webapp/you/sight/zhenjiang13.html?from=http%3A%2F%2Fm.ctrip.com%2Fwebapp%2Fyou%2Fplace%2F13.html',
+		'http://m.ctrip.com/webapp/hotel/hoteldetail/485097.html?days=1&atime=20150801&contrl=2&num=1&biz=1',
+		'http://m.ctrip.com/webapp/hotel/hoteldetail/436337.html?days=1&atime=20150801&contrl=2&num=1&biz=1',
+		'http://m.douban.com/note/510096555/?refer=home',
+	];
+	
+	window.pics = [
+		'img/city.jpg',
+		'img/2.jpg',
+		'img/3.jpeg',
+		'img/4.jpg',
+		'img/5.jpeg'
+	];
+	
+})(window, $);
