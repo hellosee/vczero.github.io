4fe66d415f2855054535e2451960a764b4f9a8e0
lh_wang <lh_wang@Ctrip.com>
2015-08-09 16:13:34 +0800

    add  search list

--- meitu/js/home/search.js
+++ meitu/js/home/search.js
@@ -2,16 +2,37 @@
 define('home/search', function(require, exports, module){
 	var $ = require('lib/zepto_pj');
 	var input = $('#search_input_text');
-	var router = [
-		'上海 到 扬州 两日游',
-		'上海 到 常州 一日游',
-		'上海 到 衢州 一日游'
-	];
+	var tpl = $('#tpl_search').html();
+	var container = $('#SEARCH_TPL');
 	
+	
+	var data = {
+		items:[
+			{
+				router: '上海到扬州',
+				desc: '两日游'
+			},
+			{
+				router: '上海到常州',
+				desc: '一日游'
+			},
+			{
+				router: '上海到衢州',
+				desc: '两日游'
+			},
+			{
+				router: '郑州到青岛',
+				desc: '三日游'
+			}
+		]
+	};
+
 	
 	//focus事件
 	input.on('focus', function(){
-		
+		var html = _.template(tpl)(data);
+		container.html(html);
+		container.fadeIn();
 	});
 	
 	
